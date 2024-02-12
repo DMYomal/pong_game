05754d4f75d807d883d741163b11871a00844ba4
DMYomal <yomalsd123@gmail.com>
2024-02-12 13:05:56 +0000

    scoreboard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,6 +13,8 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
+scoreboard.update_scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up,"Up")
@@ -21,11 +24,25 @@ screen.onkey(l_paddle.go_down,"s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)Ball
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+
+    if ball.xcor() > 320 and ball.distance(r_paddle) < 50 or ball.xcor() < -320 and ball.distance(l_paddle) < 50:
+        ball.bounce_x()
+
+    #detect r paddle miss the ball
+    elif ball.xcor() > 380:
+        ball.reset_postion()
+        scoreboard.l_point()
+
+    # detect l paddle miss the ball
+    elif ball.xcor() < -380:
+        ball.reset_postion()
+        scoreboard.r_point()
+
 
 screen.exitonclick()
 
